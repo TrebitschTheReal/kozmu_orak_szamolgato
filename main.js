267d908c5c982b1b658739a6e8d3ae1f07cca1e2
Viktor Dohanyos <vorkapp@gmail.com>
2020-04-11 17:57:55 +0200

    update

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 const unitPriceGas = 120;
 const unitPriceEl = 40;
 const unitPriceWaterAndCan = 1200;
+const dumpFor12Days = 250;
 
-const marchElStatus = 1119;
+const marchElStatus = 986;
 const marchGasStatus = 1127;
-const marchWaterStatus = 208;
+const marchWaterStatus = 197;
 
 const displayGasPrice = $('#priceGas');
 const displayElPrice = $('#priceEl');
 const displayWaterCanPrice = $('#priceWaterCan');
+const displayDumpPrice = $('#priceDump');
 
 const calculateButton = $('#btnCalculate');
 const resetButton = $('#btnReset');
@@ -26,6 +28,7 @@ const inputMarchWaterCanState = $('#marchWaterCanState');
 const elResultModal = $('#elResult');
 const gasResultModal = $('#gasResult');
 const waterCanResultModal = $('#waterCanresult');
+const dumpResultModal = $('#dumpResult');
 const sumResultModal = $('#sum');
 
 // ######################## Onready ########################
@@ -35,6 +38,7 @@ $(function () {
    displayElPrice.text(unitPriceEl);
    displayGasPrice.text(unitPriceGas);
    displayWaterCanPrice.text(unitPriceWaterAndCan);
+   displayDumpPrice.text(dumpFor12Days);
 
    inputMarchElState.val(marchElStatus);
    inputMarchElState.attr('disabled', true);
@@ -54,11 +58,13 @@ calculateButton.click(function () {
    let elResult = (inputActualElState.val() - inputMarchElState.val()) * unitPriceEl;
    let gasResult = (inputActualGasState.val() - inputMarchGasState.val()) * unitPriceGas;
    let waterCanResult = (inputActualWaterCanState.val() - inputMarchWaterCanState.val()) * unitPriceWaterAndCan;
-   let sum = elResult + gasResult + waterCanResult;
+   let dumpResult = 250;
+   let sum = elResult + gasResult + dumpResult + waterCanResult;
 
    elResultModal.text('Elmű: ' + elResult + ' Ft');
    gasResultModal.text('Gáz: ' + gasResult + ' Ft');
    waterCanResultModal.text('Víz + csatorna: ' + waterCanResult + ' Ft');
+   dumpResultModal.text('Szemét: ' + dumpResult + ' Ft');
    sumResultModal.text('Összesen: ' + sum + ' Ft')
 });
 
